e5897ed79cf01905329d743185047495067f30c3
jack <jack.goldsbrough@outlook.com>
2025-09-23 15:56:24 +0200

    ADDED VLSM and Sumnetzmaske spiel

--- netzwerk_berechnen.c
+++ netzwerk_berechnen.c
@@ -112,7 +112,7 @@ void *IP_raum_berechnen(uint8_t *ip, uint8_t subNetz, uint8_t *NetzID, uint8_t *
         if (i != 3)
             printf(".");
     }
-    printf("\n");
+    printf(" /%d\n", subNetz);
     printf("NetzID    - Broadcast\n");
     printf("   +1   -   -1         *Nutzbaren IP's\n");
 }
@@ -142,6 +142,31 @@ void subnetz_next(uint8_t *nextNetzID, uint8_t *perviousBC)
         }
     }
 }
+void IP_counter(int amount, uint8_t *ip)
+{
+    for (int i = 0; i < amount; i++)
+    {
+        ip[3] += 1;
+        if (ip[3] == 0)
+        {
+            ip[2] += 1;
+            if (ip[2] == 0)
+            {
+                ip[1] += 1;
+                if (ip[1] == 0)
+                {
+                    ip[0] += 1;
+                    if (ip[0] == 0)
+                    {
+                        printf("ERROR is finding next NetzID");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 void subnetz_berechnen(uint8_t *ip, uint8_t subNetz, int subNetzMenge, uint8_t *NetzID, uint8_t *BC)
 {
@@ -184,6 +209,128 @@ void subnetz_berechnen(uint8_t *ip, uint8_t subNetz, int subNetzMenge, uint8_t *
     }
 }
 
+int host_anzahl(uint8_t *netzID, uint8_t *BC)
+{
+    int a, b, c, d;
+    a = BC[0] - netzID[0] +1;
+    b = BC[1] - netzID[1] +1;
+    c = BC[2] - netzID[2] +1;
+    d = BC[3] - netzID[3] +1;
+
+    return a * b * c * d;
+}
+
+void VLSM_berechnen(uint8_t *ip, uint8_t subNetz, int superNetzMenge, uint8_t *NetzID, uint8_t *BC)
+{
+    IP_raum_berechnen(ip, subNetz, NetzID, BC);
+
+    int ebenen;
+    bool correct = false;
+    while(!correct)
+    {
+        printf("Wieviel Ebenen willst du VLSM machen?\n");
+        scanf("%d", &ebenen);
+        getchar();
+
+        if (ebenen > 0) correct = true;
+        else printf("UNGUELTIG Eingabe\n");
+    }
+
+    uint8_t firstNetzID[4];
+    for (int i = 0; i < 4; i++)
+    {
+        firstNetzID[i] = NetzID[i];
+    }
+
+    long gesamtHosts = host_anzahl(NetzID, BC);
+
+    int *hostsAnzahlProEbenen = malloc(ebenen * sizeof(int));
+
+    for (int i = 0; i < ebenen; i++)
+    {
+        hostsAnzahlProEbenen[i] = gesamtHosts / 2;
+        gesamtHosts /= 2;
+    }
+
+    int currentSubNetz = subNetz;
+    int counter = 1;
+    for (int i = 0; i < ebenen; i++)
+    {
+        uint8_t tempNetzID[4];
+        for(int i = 0; i < 4; i++)
+        {
+            tempNetzID[i] = firstNetzID[i];
+        }
+
+        IP_counter((hostsAnzahlProEbenen[i]), tempNetzID);
+
+        currentSubNetz++;
+        printf("--------------------\nNetzraum %d\n", counter++);
+        IP_raum_berechnen(tempNetzID, currentSubNetz, NetzID, BC);
+        printf("--------------------\n");
+        BC[3] -= 1;
+    }
+    printf("--------------------\nNetzraum %d\n", counter++);
+    IP_raum_berechnen(firstNetzID, currentSubNetz, NetzID, BC);
+    printf("--------------------\n");
+
+
+}
+
+
+void subnetzmaske_calculator(int subnetz, uint8_t *subnetzmaske)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (subnetz >= 8)
+        {
+            subnetzmaske[i] = 255;
+            subnetz -= 8;
+        }
+        else
+        {
+            if (subnetz > 0)
+            {
+                subnetzmaske[i] = 256 - (int)pow(2, 8 - subnetz);
+                subnetz = 0;
+            }
+            else
+                subnetzmaske[i] = 0;
+        }
+    }
+}
+
+void subnetzmaske_training()
+{
+    int fragen = 5;
+    int richtig = 0;
+
+    printf("Schreibst du ein Antwort: n.n.n.n \n");
+    for (int i = 0; i < fragen; i++)
+    {
+        int random = rand() % 32 + 1;
+        uint8_t subnetzmaske[4];
+        subnetzmaske_calculator(random, subnetzmaske);
+        printf("Was ist die Subnetzmaske für /%d?\n", random);
+        uint8_t eingabe[4];
+        scanf("%hhu.%hhu.%hhu.%hhu", &eingabe[0], &eingabe[1], &eingabe[2], &eingabe[3]);
+        getchar();
+
+        if (eingabe[0] == subnetzmaske[0] && eingabe[1] == subnetzmaske[1] && eingabe[2] == subnetzmaske[2] && eingabe[3] == subnetzmaske[3])
+        {
+            printf("Richtig!\n");
+            richtig++;
+        }
+        else
+        {
+            printf("Falsch! Die richtige Antwort ist %d.%d.%d.%d\n", subnetzmaske[0], subnetzmaske[1], subnetzmaske[2], subnetzmaske[3]);
+        }
+
+    }
+
+
+    printf("Du hast %d von %d richtig beantwortet!\n", richtig, fragen);
+}
 
 void set_IP(char *IP, uint8_t *ip)
 {
@@ -299,7 +446,8 @@ int main()
     {
         options = 0;
 
-        printf("\n1 - IP-Raum Berechnen\n2 - Subnetze Berechnen\n9 - exit\n");
+        printf("\n1 - IP-Raum Berechnen\n2 - Subnetze Berechnen\n3 - VLSM (Variable Length Subnet Masking)\n"
+               "4 - Kennst du ein subNetz Maske?\n9 - exit\n");
         scanf("%d", &options);
         getchar();
 
@@ -327,6 +475,23 @@ int main()
 
             IP_raum_berechnen(ip, subNetz, NetzID, BC);
             subnetz_berechnen(ip, subNetz, subNetzMenge, NetzID, BC);
+        }
+        else if (options == 3)
+        {
+            uint8_t NetzID[4];
+            uint8_t BC[4];
+            uint8_t subNetz;
+            uint8_t ip[4];
+            int subNetzMenge;
+            set_IP_subnetz(ip, &subNetz);
+
+            VLSM_berechnen(ip, subNetz, subNetzMenge, NetzID, BC);
+
+
+        }
+        else if (options == 4)
+        {
+            subnetzmaske_training();
         }
         else if (options == 9)
         {
